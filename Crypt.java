c4d9c6a3b6860c94f15c12fff66461aedbfe535a
{yoshinotaichi} <{yoshinotaichi@gmail.com}>
2017-02-09 17:54:19 +0900

    caesar encryption done.

--- Crypt.java
+++ Crypt.java
@@ -35,17 +35,21 @@ class WindowMan implements ActionListener{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// パーツをつくる
+		//   元テキスト
 		taPlain = new JTextArea(5, 20);
 		spPlain = new JScrollPane(taPlain);
 
+		//   ボタン
 		buttonEncrypt = new JButton("Encrypt");
 		buttonEncrypt.addActionListener(this);
 		panelButton = new JPanel();
 		panelButton.add(buttonEncrypt);
 
+		//   暗号化テキスト
 		taEncrypted = new JTextArea(5, 20);
 		spEncrypted = new JScrollPane(taEncrypted);
 
+		// 載せる
 		Container con = frame.getContentPane();
 		con.setLayout(new GridLayout(3, 1));
 		con.add(spPlain);
@@ -56,13 +60,41 @@ class WindowMan implements ActionListener{
 		frame.setVisible(true);
 	}
 
-	// actionPerformed(){...}
-	pubilc void actionPerformed(ActionEvent){
-		
+	// ボタンをクリックした時の処理
+	public void actionPerformed(ActionEvent ae){
+		String textPlain = taPlain.getText();
+		CryptMan cman = new CryptMan();
+		String textEncrypted = cman.getTextEncrypted(textPlain);
+
+		taEncrypted.setText(textEncrypted);
 	}
 }
 
 
 class CryptMan {
+	// field
+	private int numCaesar;
+	private char[] resource;
 
+	// method
+	// コンストラクタ
+	public CryptMan() {
+		this.numCaesar = +1;
+	}
+
+	// テキストを、シーザー暗号化する
+	public String getTextEncrypted(String str) {
+		// テキストを、文字の配列にする
+		this.resource = str.toCharArray();
+
+		// 一文字ずつ、暗号化する
+		for(int i = 0;  i < str.length();  i++) {
+			int tmp = this.resource[i];
+			tmp += this.numCaesar;
+			this.resource[i] = (char)tmp;
+		}
+
+		// 暗号化されたテキストを返す
+		return String.valueOf(this.resource);
+	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # crypt
 暗号化・復号化プログラムのテスト
+
+## cryptman
+- file: crypt.java
+- class:
+	- crypt
+	- cryptman
+		- encrypt()
+		- decrypt()
